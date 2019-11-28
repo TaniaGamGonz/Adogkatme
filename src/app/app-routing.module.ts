a9da7a9684c2c0553965d71edb4a28bb656e1a90
Tania Gamero González <tagago@MacBook-Pro-de-David.local>
2019-11-28 15:22:56 +0100

    Menu desplegable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { LoginComponent } from './login/login/login.component';
+import { ProtectiveSearchComponent } from './protective/protective-search/protective-search.component';
 
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'protectoras', component: ProtectiveSearchComponent},
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
